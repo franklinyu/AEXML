4a1e05f73e10948b0fb645ce84253f002d11f1d9
Franklin Yu <franklinyu@hotmail.com>
2016-06-08 13:39:01 -0400

    eliminate errors
    *   pass compilation
    *   do not solve warnings
    *   do not pass tests

--- Tests/AEXMLTests.swift
+++ Tests/AEXMLTests.swift
@@ -36,14 +36,14 @@ class AEXMLTests: XCTestCase {
     // MARK: - Helpers
     
     func URLForResource(fileName: String, withExtension: String) -> NSURL {
-        let bundle = NSBundle(forClass: AEXMLTests.self)
-        return bundle.URLForResource(fileName, withExtension: withExtension)!
+        let bundle = NSBundle(for: AEXMLTests.self)
+        return bundle.urlForResource(fileName, withExtension: withExtension)!
     }
     
     func xmlDocumentFromURL(url: NSURL) -> AEXMLDocument {
         var xmlDocument = AEXMLDocument()
         
-        if let data = NSData(contentsOfURL: url) {
+        if let data = NSData(contentsOf: url) {
             do {
                 xmlDocument = try AEXMLDocument(xmlData: data)
             } catch {
@@ -55,8 +55,8 @@ class AEXMLTests: XCTestCase {
     }
     
     func readXMLFromFile(filename: String) -> AEXMLDocument {
-        let url = URLForResource(filename, withExtension: "xml")
-        return xmlDocumentFromURL(url)
+        let url = URLForResource(fileName: filename, withExtension: "xml")
+        return xmlDocumentFromURL(url: url)
     }
     
     // MARK: - Setup & Teardown
@@ -65,8 +65,8 @@ class AEXMLTests: XCTestCase {
         super.setUp()
         
         // create some sample xml documents
-        exampleXML = readXMLFromFile("example")
-        plantsXML = readXMLFromFile("plant_catalog")
+        exampleXML = readXMLFromFile(filename: "example")
+        plantsXML = readXMLFromFile(filename: "plant_catalog")
     }
     
     override func tearDown() {
@@ -222,7 +222,7 @@ class AEXMLTests: XCTestCase {
         cats.addChild(name: "cat", value: "Tinna")
         
         var count = 0
-        if let tinnas = cats["cat"].allWithValue("Tinna") {
+        if let tinnas = cats["cat"].allWithValue(value: "Tinna") {
             for _ in tinnas {
                 count += 1
             }
@@ -232,7 +232,7 @@ class AEXMLTests: XCTestCase {
     
     func testAllWithAttributes() {
         var count = 0
-        if let bulls = exampleXML.root["dogs"]["dog"].allWithAttributes(["color" : "white"]) {
+        if let bulls = exampleXML.root["dogs"]["dog"].allWithAttributes(attributes: ["color" : "white"]) {
             for _ in bulls {
                 count += 1
             }
@@ -318,13 +318,13 @@ class AEXMLTests: XCTestCase {
     // MARK: - XML Parse Performance
     
     func testReadXMLPerformance() {
-        self.measureBlock() {
-            _ = self.readXMLFromFile("plant_catalog")
+        self.measure() {
+            _ = self.readXMLFromFile(filename: "plant_catalog")
         }
     }
     
     func testWriteXMLPerformance() {
-        self.measureBlock() {
+        self.measure() {
             _ = self.plantsXML.xmlString
         }
     }
